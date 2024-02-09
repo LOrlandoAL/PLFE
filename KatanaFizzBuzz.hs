7cc6a3c7c631867cb15a639627465e54fdd32cd0
Luis Orlando Andrade Lemus <122890598+LOrlandoAL@users.noreply.github.com>
2024-02-08 19:32:35 -0600

    Create KatanaFizzBuzz.hs

---
+++ KatanaFizzBuzz.hs
@@ -0,0 +1,39 @@
+module FizzBuzz where
+
+fizzBuzz :: Int -> String
+fizzBuzz n
+  | n `mod` 3 == 0 && n `mod` 5 == 0 = "fizzbuzz!"
+  | n `mod` 3 == 0                   = "fizz!"
+  | n `mod` 5 == 0                   = "buzz!"
+  | otherwise                        = number n ++ "!"
+
+lessThan20 :: Int -> String
+lessThan20 n
+  | n > 0 && n < 20 =
+    let lessThan20Answers =
+          words ("one two three four five six seven eight nine ten " ++
+                 "eleven twelve thirteen fourteen fifteen sixteen "  ++
+                 "seventeen eighteen nineteen")
+    in lessThan20Answers !! (n-1)
+  | otherwise = ""
+
+tens :: Int -> String
+tens n
+  | n >= 2 && n <= 9 =
+    let tensAnswers = words "twenty thirty forty fifty sixty seventy eighty ninety"
+    in tensAnswers !! (n-2)
+  | otherwise = ""
+
+number :: Int -> String
+number n
+  | 1 <= n && n < 20           = lessThan20 n
+  | n `mod` 10 == 0 && n < 100 = tens (n `div` 10)
+  | n < 100                    = tens (n `div` 10) ++ " " ++ lessThan20 (n `mod` 10)
+  | n == 100                   = "one hundred"
+  | otherwise                  = ""
+
+
+-- Zona de Testeo
+main :: IO ()
+main = do
+    print $ fizzBuzz 100
